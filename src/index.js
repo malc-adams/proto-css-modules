80695534d5a77690e1bd0159fee0b6ee486bfb25
malc-adams <47540589+malc-adams@users.noreply.github.com>
2022-07-02 15:11:15 +0100

    Add a style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import style from './css/index.module.scss';
+
 function component() {
   const element = document.createElement('div');
+  element.classList.add(style.hello);
   element.innerHTML = "proto-css-modules";
   return element;
 }
