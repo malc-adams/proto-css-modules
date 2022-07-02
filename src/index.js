746e650658487591922e3ef391db8991e8f040e3
malc-adams <47540589+malc-adams@users.noreply.github.com>
2022-07-02 16:50:31 +0100

    Add elements

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import style from './css/index.module.scss';
+import style from './css/index.scss';
 
-function component() {
+function init() {
+  const grid = createGrid();
+  return grid;
+}
+
+function createGrid() {
   const element = document.createElement('div');
-  element.classList.add(style.hello);
-  element.innerHTML = "proto-css-modules";
+  element.classList.add(style.grid);
+
+  // Create the grid items.
+  for (let i = 0; i < 3; i++) {
+    const item = createGridItem();
+    element.appendChild(item);
+  }
+
   return element;
 }
 
-document.body.appendChild(component());
+function createGridItem() {
+  const element = document.createElement('div');
+  element.classList.add(style.item);
+  return element;
+}
+
+document.body.appendChild(init());
