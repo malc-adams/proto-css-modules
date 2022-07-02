41e3f98eed3137b7a71b476577a2178d0ca624d8
malc-adams <47540589+malc-adams@users.noreply.github.com>
2022-07-02 18:00:46 +0100

    Working strategy

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ import style from './css/index.scss';
 
 function init() {
   const element = document.createElement('div');
-  const configList = ['bp-1-2-3', 'bp-2-3', 'bp-3'];
+  const grid1 = createGrid(['bp-1-1', 'bp-2-2', 'bp-3-3']);
+  const grid2 = createGrid(['bp-2-1', 'bp-3-2']);
+  const grid3 = createGrid(['bp-3-1']);
 
-  for (let i = 0; i < configList.length; i++) {
-    element.appendChild(createGrid(configList[i]));
-  }
+  element.appendChild(grid1);
+  element.appendChild(grid2);
+  element.appendChild(grid3);
 
   return element;
 }
 
 function createGrid(config) {
+  let i;
   const configWrapper = document.createElement('div');
-  configWrapper.classList.add(style[config]);
+
+  for (i = 0; i < config.length; i++) {
+    configWrapper.classList.add(style[config[i]]);
+  }
 
   const element = document.createElement('div');
   element.classList.add(style.grid);
@@ -21,7 +27,7 @@ function createGrid(config) {
   configWrapper.appendChild(element);
 
   // Create the grid items.
-  for (let i = 0; i < 3; i++) {
+  for (i = 0; i < 3; i++) {
     const item = createGridItem();
     element.appendChild(item);
   }
