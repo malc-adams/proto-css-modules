dfff978d2af58a66604b4e5ec5c99737c19217a4
malc-adams <47540589+malc-adams@users.noreply.github.com>
2022-07-02 23:58:46 +0100

    Complete example

--- src/index.js
+++ src/index.js
@@ -4,34 +4,34 @@ function init() {
   const element = document.createElement('div');
 
   // Create the unique modules.
-  const grid1 = createGrid(['bp-1-2', 'bp-2-3', 'bp-3-4', 'bp-4-5']);
-  const grid2 = createGrid(['bp-2-2', 'bp-3-3', 'bp-4-4']);
-  const grid3 = createGrid(['bp-3-2', 'bp-4-3']);
+  const grid1 = createGrid(['bp-1-2', 'bp-2-3', 'bp-3-4']);
+  const grid2 = createGrid(['bp-2-2', 'bp-3-3']);
+
+  grid1.classList.add(style.gridWrapper, style.fullWidth);
+  grid2.classList.add(style.gridWrapper, style.partWidth);
 
   element.appendChild(grid1);
   element.appendChild(grid2);
-  element.appendChild(grid3);
 
   return element;
 }
 
-function createGrid(config) {
+function createGrid(config = []) {
   let i;
   const configWrapper = document.createElement('div');
 
   for (i = 0; i < config.length; i++) {
     configWrapper.classList.add(style[config[i]]);
   }
+  const grid = document.createElement('div');
+  grid.classList.add(style.grid);
 
-  const element = document.createElement('div');
-  element.classList.add(style.grid);
-
-  configWrapper.appendChild(element);
+  configWrapper.appendChild(grid);
 
   // Create the grid items.
-  for (i = 0; i < 3; i++) {
+  for (i = 0; i < 4; i++) {
     const item = createGridItem();
-    element.appendChild(item);
+    grid.appendChild(item);
   }
 
   return configWrapper;
@@ -40,7 +40,13 @@ function createGrid(config) {
 function createGridItem() {
   const element = document.createElement('div');
   element.classList.add(style.item);
-  return element;
+
+  const col = document.createElement('div');
+  col.classList.add(style.col);
+
+  col.appendChild(element);
+
+  return col;
 }
 
 document.body.appendChild(init());
