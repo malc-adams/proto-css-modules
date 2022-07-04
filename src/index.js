ec0fcb1df929e8afccf47c4734e836f09528da99
malc-adams <47540589+malc-adams@users.noreply.github.com>
2022-07-04 23:18:44 +0100

    Structured

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import style from './css/index.scss';
+import pageStyle from './css/index.scss';
+import moduleStyle from './css/module.scss';
 
 function init() {
   const element = document.createElement('div');
@@ -6,10 +7,10 @@ function init() {
   // Create the unique modules.
   const grid1 = createGrid(['b1-l2', 'b2-l3', 'b3-l4']);
   const grid2 = createGrid(['b2-l2', 'b3-l3']);
-
-  grid1.classList.add(style.gridWrapper, style.fullWidth);
-  grid2.classList.add(style.gridWrapper, style.partWidth);
-
+  
+  grid1.classList.add(pageStyle.gridWrapper, pageStyle.fullWidth);
+  grid2.classList.add(pageStyle.gridWrapper, pageStyle.partWidth);
+  
   element.appendChild(grid1);
   element.appendChild(grid2);
 
@@ -21,10 +22,10 @@ function createGrid(config = []) {
   const configWrapper = document.createElement('div');
 
   for (i = 0; i < config.length; i++) {
-    configWrapper.classList.add(style[config[i]]);
+    configWrapper.classList.add(moduleStyle[config[i]]);
   }
   const grid = document.createElement('div');
-  grid.classList.add(style.grid);
+  grid.classList.add(moduleStyle.grid);
 
   configWrapper.appendChild(grid);
 
@@ -39,10 +40,10 @@ function createGrid(config = []) {
 
 function createGridItem() {
   const element = document.createElement('div');
-  element.classList.add(style.item);
+  element.classList.add(moduleStyle.item);
 
   const col = document.createElement('div');
-  col.classList.add(style.col);
+  col.classList.add(moduleStyle.col);
 
   col.appendChild(element);
 
